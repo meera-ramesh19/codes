b9e888eca3575e9e48e537efe1f718a7bb60f47f
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-02-20 16:09:39 -0500

    Create Divide and Conquer.js

---
+++ 7kyu/Divide and Conquer.js
@@ -0,0 +1,24 @@
+Divide and Conquer
+
+Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+
+Return as a number.
+
+FUNDAMENTALSSTRINGSNUMBERSARRAYS
+
+
+function divCon(data){
+  let stringCount=0,count=0,numCount=0;
+    for (var i = 0; i <= data.length; i++) {
+      
+        if (typeof data[i]==='number'){
+           numCount+=data[i]
+   }
+      
+      if (typeof data[i]==='string'){ 
+        count=Number(data[i])
+        stringCount+=count                      
+          console.log(count)}
+      }
+     return  numCount-stringCount
+  }
