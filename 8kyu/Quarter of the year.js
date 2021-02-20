9e362912488fb5dec74a1c37e7360d541fc73b8e
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-02-20 15:23:31 -0500

    Create  Quarter of the year.js

---
+++ 8kyu/Quarter of the year.js
@@ -0,0 +1,18 @@
+
+Quarter of the year
+
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+FUNDAMENTALS
+
+
+const quarterOf = (month) => {
+  // Your code here
+  let result =month<=3?  1
+         : month<=6?  2
+         : month<=9? 3
+         :4
+  return result;
+}
