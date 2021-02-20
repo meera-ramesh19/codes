4d43d253ce4f6e87a5bf8e8f128c262a5f59b582
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-02-20 15:52:01 -0500

    Create Grasshopper - Variable Assignment Debug.js

---
+++ 8kyu/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,14 @@
+Grasshopper - Variable Assignment Debug
+
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+FUNDAMENTALS
+
+
+
+
+let  a = "dev"
+let  b = "Lab"
+
+let name = a + b
