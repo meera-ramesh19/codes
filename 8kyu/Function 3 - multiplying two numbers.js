e22accd01a41c21e4225750d0e880db71ebfd48b
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-02-20 15:48:29 -0500

    Create Function 3 - multiplying two numbers.js

---
+++ 8kyu/Function 3 - multiplying two numbers.js
@@ -0,0 +1,14 @@
+
+
+Function 3 - multiplying two numbers
+
+Implement a function which multiplies two numbers.
+
+FUNDAMENTALS
+
+
+
+// Write here your multiply-function
+function multiply(a,b){
+  return a*b
+}
