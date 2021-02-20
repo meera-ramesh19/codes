0e826dc8bddf467b37440e9d42d56bdf95b02183
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-02-20 15:33:59 -0500

    Create FIXME: Replace all dots.js

---
+++ 8kyu/FIXME: Replace all dots.js
@@ -0,0 +1,22 @@
+FIXME: Replace all dots
+
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+
+BUGS
+
+
+
+var replaceDots = function(str) {
+  let strin = str.split('.').join('-')
+  if( str==='' || str===null)
+    { return ''}
+  return strin
+}
