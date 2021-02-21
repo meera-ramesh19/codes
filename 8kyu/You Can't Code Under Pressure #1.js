a61f0277093d3ca8094417563ec20f1d84097380
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-02-20 20:26:41 -0500

    Create You Can't Code Under Pressure #1.js

---
+++ 8kyu/You Can't Code Under Pressure #1.js
@@ -0,0 +1,14 @@
+
+You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the integer and return it.
+
+FUNDAMENTALSNUMBERSINTEGERS
+
+
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  
+  return i+i;
+}
