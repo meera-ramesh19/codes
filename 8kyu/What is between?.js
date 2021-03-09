76e05ec50f4d9775ebe3966d04018634eff9a30c
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-03-08 19:58:49 -0500

    Create What is between?.js

---
+++ 8kyu/What is between?.js
@@ -0,0 +1,20 @@
+
+What is between?
+
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+FUNDAMENTALSALGORITHMS
+
+function between(a, b) {
+  // your code here
+  let arr=[]
+  for(let i=a;i<=b;i++){
+    arr.push(i)
+  }
+  return arr
+}
