244710cdc666327b10dacb81f94f9e5355343d75
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-03-08 19:24:08 -0500

    Create  Classic Hello World.js

---
+++ 8kyu/Classic Hello World.js
@@ -0,0 +1,23 @@
+
+Classic Hello World
+
+You are given a method called main, make it print Hello World! and don't return anything
+
+Note that for some languages, the function main is the entry point of the program.
+
+Here's how it will be tested:
+
+    Solution.main("parameter1","parameter2");
+Hints:
+
+Check your references
+Think about the scope of your method
+For prolog you can use write but there are better ways
+FUNDAMENTALSIMPERATIVE PROGRAMMING
+
+// Print "Hello World!" to the screen
+class Solution{
+  static main(){
+    console.log('Hello World!')
+  }
+}
