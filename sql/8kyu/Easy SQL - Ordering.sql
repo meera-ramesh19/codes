4028ce40a90d05ca18e57b67c6e2fb13fddb7f89
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-03-10 07:10:08 -0500

    Create Easy SQL - Ordering.sql

---
+++ sql/8kyu/Easy SQL - Ordering.sql
@@ -0,0 +1,20 @@
+
+Easy SQL - Ordering
+
+Your task is to sort the information in the provided table 'companies' by number of employees (high to low). Returned table should be in the same format as provided:
+
+companies table schema
+
+id
+ceo
+motto
+employees
+Solution should use pure SQL. Ruby is only used in test cases.
+
+FUNDAMENTALSSQLDATABASESINFORMATION SYSTEMSDATA
+
+
+
+/*  SQL  */
+select id,ceo,motto,employees from companies
+order by employees desc
