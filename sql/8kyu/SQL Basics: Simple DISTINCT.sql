6031088b88a40ecab946852aa602ee26b7ed4031
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-04-03 00:40:42 -0400

    Create  SQL Basics: Simple DISTINCT.sql

---
+++ sql/8kyu/SQL Basics: Simple DISTINCT.sql
@@ -0,0 +1,38 @@
+
+
+SQL Basics: Simple DISTINCT
+201887% of 1,41010,848 of 10,851matt c
+SQL
+PostgreSQL 9.6
+VIM
+EMACS
+Instructions
+Output
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+people table schema
+id
+name
+age
+select table schema
+age (distinct)
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+
+FUNDAMENTALSSQLDATABASESINFORMATION SYSTEMSDATA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select distinct age from people
