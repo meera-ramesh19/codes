ce7e54f00f51908334e5ac0b1adbd24aea275f96
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-04-23 00:57:21 -0400

    Create  Evens times last.js

---
+++ 7kyu/Evens times last.js
@@ -0,0 +1,19 @@
+
+Evens times last
+
+Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+If the sequence is empty, you should return 0.
+
+FUNDAMENTALSARRAYS
+
+
+
+const evenLast = numbers => {
+  if (numbers.length === 0) return 0
+
+  const lastInteger = numbers[numbers.length - 1]
+  const sum = numbers.reduce((acc, number, index) => (index % 2 === 0 ? acc + number : acc), 0)
+
+  return sum * lastInteger
+}
