0f4e59f73c7aaca08726034e74c38336dce115ef
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-05-19 01:02:06 -0400

    Update Evens times last.js

--- 7kyu/Evens times last.js
+++ 7kyu/Evens times last.js
@@ -7,6 +7,13 @@ If the sequence is empty, you should return 0.
 
 FUNDAMENTALSARRAYS
 
+function evenLast(numbers) {
+  // Good luck
+ 
+ const res=numbers.reduce((acc,val,index)=> (index%2===0 && !index.length)? acc+val:acc,0)
+  return res*numbers.slice(-1)
+}
+
 
 
 const evenLast = numbers => {
