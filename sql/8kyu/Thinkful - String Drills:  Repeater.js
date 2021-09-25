be402b76f3d78c900f9ee8fd1072d93b089daf7b
Meera <46873861+meera-ramesh19@users.noreply.github.com>
2021-09-25 16:30:32 -0400

    Create Thinkful - String Drills:  Repeater.js

---
+++ sql/8kyu/Thinkful - String Drills:  Repeater.js
@@ -0,0 +1,33 @@
+TThinkful - String Drills: Repeater
+
+Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+Example:
+Repeater.repeat("a", 5)
+should return
+
+"aaaaa"
+FUNDAMENTALSfunction repeater(string, n){
+  //Your code goes here.
+  let str=''
+  for(let i=0;i<n;i++){
+    str+=string
+  }
+ return str
+}
+
+Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+Example:
+Repeater.repeat("a", 5)
+should return
+
+"aaaaa"
+FUNDAMENTALSfunction repeater(string, n){
+  //Your code goes here.
+  let str=''
+  for(let i=0;i<n;i++){
+    str+=string
+  }
+ return str
+}
